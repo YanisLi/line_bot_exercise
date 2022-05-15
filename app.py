7c88951fc7f695368dd1e398a2d860e9833cd7dc
YanisLi <chuliyan.taiwan@gmail.com>
2022-05-15 17:09:08 +0800

    dice_v0.6.7

--- app.py
+++ app.py
@@ -39,11 +39,15 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    rep = '回覆' #+ str.random.randint(1,6)
+    dice_rep = dice_bot(msg, dice_rep)
+    rep = '回覆' + dice_rep
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text = rep))
 
-
+def dice_bot(msg, dice_rep):
+    if msg in 'dice':
+        dice_rep = random.randint(1,6)
+        
 if __name__ == "__main__":
     app.run()
