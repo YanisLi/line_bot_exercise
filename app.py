9dc165b965ea4f878214d5ab7afb329bc60fb5bc
YanisLi <chuliyan.taiwan@gmail.com>
2022-05-15 16:41:40 +0800

    dice_v0.6.4

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('vnuB8TsyvyjrlPPI6fk7XU8vuw3ZGLVDXYuYTmpcScSwGiw/6/hvTC5O82hPIMhY2WjQmPZBXL5B8OnXmX+WYSkbdHUs3DY7tVjPcPRDLG3Z+I+JcFAl59pmn/h+6AcPCRTibGWjdXAEmmXxrqQz3QdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('a0f2480fd9735408577c61ea51b81855')
 
 
 @app.route("/callback", methods=['POST'])
@@ -37,6 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
